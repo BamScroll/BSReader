eaebbfb87ba7830b1712e3f1b53b0a3070fc2330
BamScroll <bamscroll@163.com>
2024-04-08 00:26:31 +0800

    🎉 init: 初次提交

---
+++ BSReader/BSReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BSReaderApp.swift
+//  BSReader
+//
+//  Created by BamScroll on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct BSReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
